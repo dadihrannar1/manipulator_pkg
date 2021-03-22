37c8a2a82d497da9cb5db40f1b6ba12967bfb451
dadihrannar1 <ddavid20@student.aau.dk>
2021-03-22 11:11:21 +0100

    first commit

---
+++ src/GUI.cpp
@@ -0,0 +1,22 @@
+#include <iostream>            
+#include <ros/ros.h>
+
+int main(int argc, char const *argv[])
+{
+
+int black = 1;
+int blue = 2;
+int white = 3;
+int curved = 4;
+int flat = 5;
+
+std::cout << "Please pick which cover you want" << std::endl;
+
+std::cin 
+
+
+
+
+
+    return 0;
+}
